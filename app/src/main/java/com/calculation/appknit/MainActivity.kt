766c49840dbeaa586a96ce6b0f57515639033642
madhusmita14 <madhusmitanayak14@gmail.com>
2021-10-25 16:05:07 +0530

    forgot password page design

---
+++ app/src/main/java/com/calculation/appknit/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.calculation.appknit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
+import com.calculation.appknit.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var forgot_password_submit_btn: RelativeLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        forgot_password_submit_btn = binding.forgotPasswordSubmitBtn
+        forgot_password_submit_btn.setOnClickListener {
+            showCustomAlert()
+        }
+    }
+
+    private fun showCustomAlert() {
+        val dialogView = layoutInflater.inflate(R.layout.forgot_password_dialogbox, null)
+        val customDialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .show()
+
+        val forgot_password_dialog_ok_btn = dialogView.findViewById<RelativeLayout>(R.id.forgot_password_dialog_ok_btn)
+        forgot_password_dialog_ok_btn.setOnClickListener {
+            customDialog.dismiss()
+        }
+    }
+}
